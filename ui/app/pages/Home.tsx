6ae12dd76afb7ad46a452e069fff6819f0539abe
MickusB <michael.brady.ie@gmail.com>
2026-03-12 09:59:52 +0100

    Reworked main page to display business hour list data in Datatable. Still needs more work, but looks much better

--- ui/app/pages/Home.tsx
+++ ui/app/pages/Home.tsx
@@ -1,29 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
 import { useCurrentTheme } from "@dynatrace/strato-components/core";
 import { Flex } from "@dynatrace/strato-components/layouts";
-import { Button } from "@dynatrace/strato-components/buttons";
 import { Heading, List, Paragraph, Strong, Text } from "@dynatrace/strato-components/typography";
-import { DataTable, convertToColumns } from '@dynatrace/strato-components-preview/tables';
+import { DataTable, DataTableColumnDef, convertToColumns } from '@dynatrace/strato-components-preview/tables';
 import { useListDocuments } from '@dynatrace-sdk/react-hooks';
+import { DocumentList } from "@dynatrace-sdk/client-document";
+
+  type BusinessHoursListRow = 
+  NonNullable<DocumentList>["documents"][number];
 
 export const Home = () => {
-  const { data:managedHostLists } = useListDocuments({
+  const { data:businessHoursLists } = useListDocuments({
     filter: `type contains 'managedHostList'`,
   })
+
+  const columns = useMemo<DataTableColumnDef<BusinessHoursListRow>[]>(() => [
+    { id: "id", accessor: "id", header: "ID" },
+    { id: "name", accessor: "name", header: "Name" },
+  ], [businessHoursLists]);
 
   const theme = useCurrentTheme();
   return (
     <Flex flexDirection="column" alignItems="normal" padding={2}>
-      <Heading>Business Hours</Heading>
+      <Heading>Business Hours Lists</Heading>
       <Flex gap={8} paddingTop={12} flexFlow="wrap">
-        {managedHostLists && (
-          <List>
-            {managedHostLists.documents.map((doc) => (
-              <Text key={doc.id}>{doc.id} {doc.name}</Text>
-            ))}
-          </List>
-        )}
+        {businessHoursLists && 
+          <DataTable data={businessHoursLists.documents} columns={columns}/>
+        }
       </Flex>
     </Flex>
   );
